f360704f8717eb71f88f79f8f33b3872524e4719
Rakesh Sankhla <75526736+rsankhla20@users.noreply.github.com>
2024-02-16 17:36:24 +0530

    frontend completed

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Home from "./pages/Home/Home";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext.jsx";
+
+function App() {
+  const { authUser } = useAuthContext();
+  return (
+    <>
+      <div className="p-4 h-screen flex items-center justify-center">
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <Home /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path="/login"
+            element={authUser ? <Navigate to="/" /> : <Login />}
+          />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to="/" /> : <Signup />}
+          />
+        </Routes>
+
+        <Toaster />
+      </div>
+    </>
+  );
+}
+
+export default App;
